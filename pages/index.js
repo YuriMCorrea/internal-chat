c356004ba436c548cca5faaa51eea0ef9454886e
YuriMCorrea <yurimcorrea@gmail.com>
2022-01-27 16:45:18 -0300

    using next routes onChange and onSubmit and conditional rendering

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React,  { useState } from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return(
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style:none;
-            }
 
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-
-            /* App fit Height */
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex:1;
-            }
-            #__next > * {
-                flex:1
-            }
-            /* ./App fit Height */
-        `}
-        </style>
-    );
-}
 
 function Titulo(props) {
     console.log(props);
@@ -55,10 +26,11 @@ function Titulo(props) {
 function PaginaInicial() {
     /* const username = 'YuriMCorrea'; */
     const [username, setUsername] = useState('');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -76,7 +48,7 @@ function PaginaInicial() {
                 xs: 'column',
                 sm: 'row',
               },
-              width: '100%', maxWidth: '800px',
+              width: '100%', maxWidth: 'fit-content',
               borderRadius: '10px', padding: '32px', margin: '16px',
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
               backgroundColor: appConfig.theme.colors.neutrals[700],
@@ -85,9 +57,15 @@ function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (eventInfo) {
+                eventInfo.preventDefault();
+                console.log("form submetido");
+                roteamento.push('/chat');
+                /* window.location.href = '/chat'; */
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                width: { xs: '100%', sm: '50%' }, minWidth:'300px', textAlign: 'center', marginBottom: '32px',
               }}
             >
               <Titulo tag="h2">Seja bem-vindo!</Titulo>
@@ -105,6 +83,7 @@ function PaginaInicial() {
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
+                placeholder='Enter your GitHub user name'
                 value={username}
                 onChange={(e) => {
                   setUsername(e.target.value)
@@ -126,41 +105,47 @@ function PaginaInicial() {
   
   
             {/* Photo Area */}
-            <Box
-              styleSheet={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                maxWidth: '300px',
-                padding: '16px',
-                backgroundColor: appConfig.theme.colors.neutrals[800],
-                border: '1px solid',
-                borderColor: appConfig.theme.colors.neutrals[999],
-                borderRadius: '10px',
-                flex: 1,
-                minHeight: '240px',
-              }}
-            >
-              <Image
-                styleSheet={{
-                  borderRadius: '50%',
-                  marginBottom: '16px',
-                }}
-                src={`https://github.com/${username}.png`}
-              />
-              <Text
-                variant="body4"
-                styleSheet={{
-                  color: appConfig.theme.colors.neutrals[200],
-                  backgroundColor: appConfig.theme.colors.neutrals[900],
-                  padding: '3px 10px',
-                  borderRadius: '1000px'
-                }}
-              >
-                {username}
-              </Text>
-            </Box>
-            {/* Photo Area */}
+            {
+              username.length > 1 ?
+                <>
+                  <Box
+                    styleSheet={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      maxWidth: '300px',
+                      padding: '16px',
+                      backgroundColor: appConfig.theme.colors.neutrals[800],
+                      border: '1px solid',
+                      borderColor: appConfig.theme.colors.neutrals[999],
+                      borderRadius: '10px',
+                      flex: 1,
+                      minHeight: '240px',
+                    }}
+                  >
+                    <Image
+                      styleSheet={{
+                        borderRadius: '50%',
+                        marginBottom: '16px',
+                      }}
+                      src={`https://github.com/${username}.png`}
+                    />
+                    <Text
+                      variant="body4"
+                      styleSheet={{
+                        color: appConfig.theme.colors.neutrals[200],
+                        backgroundColor: appConfig.theme.colors.neutrals[900],
+                        padding: '3px 10px',
+                        borderRadius: '1000px'
+                      }}
+                    >
+                      {username}
+                    </Text>
+                  </Box>
+                </>
+                : null
+              }
+              {/* Photo Area */}
           </Box>
         </Box>
       </>
