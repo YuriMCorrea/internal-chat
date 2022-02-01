378a97edaaa78ec4c4dd12f984527ba413bfeb6d
YuriMCorrea <yurimcorrea@gmail.com>
2022-02-01 15:27:48 -0300

    d

--- api.js
+++ api.js
@@ -5,14 +5,14 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
-export function getServerSideProps() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// export function getServerSideProps() {
+//   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+//   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-  return {
-    props: {
-      supabaseUrl,
-      supabaseKey,
-    },
-  };
-}
+//   return {
+//     props: {
+//       supabaseUrl,
+//       supabaseKey,
+//     },
+//   };
+// }
