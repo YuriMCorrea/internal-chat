748535157251a84966f364e210efc341c890cb46
YuriMCorrea <yurimcorrea@gmail.com>
2022-02-01 15:17:58 -0300

    dbug

--- pages/chat.js
+++ pages/chat.js
@@ -7,7 +7,6 @@ import { Box, Text, TextField, Image, Button, Icon } from '@skynexui/components'
 import { ButtonSendSticker } from '../src/components/ButtonSendSticker.js';
 import appConfig from '../config.json';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 function catchMsgRealTime(addMsg){
     return(
